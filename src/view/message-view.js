d3a6bef066a7754d285579ea599e92f63a0a7dae
Irina Lagutina <ivl77@yandex.ru>
2024-05-10 16:46:29 +0300

    Импортировала константу

--- src/view/message-view.js
+++ src/view/message-view.js
@@ -1,10 +1,12 @@
 import AbstractView from '../framework/view/abstract-view.js';
+import {DEFAULT_FILTER} from '../utils/filter.js';
+
 
 const createMessageTemplate = (err, filter) => {
   if (err) {
     return '<p class="trip-events__msg">Failed to load latest route information</p>';
   }
-  return filter === 'everything' ?
+  return filter === DEFAULT_FILTER ?
     '<p class="trip-events__msg">Click New Event to create your first point</p>' :
     `<p class="trip-events__msg">There are no ${filter} events now</p>`;
 };
