357f69895fae41121d859bc8ae7c15fb7ab82d1c
Irina Lagutina <ivl77@yandex.ru>
2024-05-27 22:15:02 +0300

    Добавили в модель методы обновления, добавления и удаления точки

--- src/model/events-model.js
+++ src/model/events-model.js
@@ -12,16 +12,43 @@ export default class EventsModel extends Observable {
   #destinations = null;
   #offers = null;
 
-  get events() {
-    return this.#events;
-  }
-
   get destinations() {
     return this.#destinations;
   }
 
   get offers() {
     return this.#offers;
+  }
+
+  get events() {
+    return this.#events;
+  }
+
+  updateEvent(updateType, update) {
+    const index = this.#events.findIndex((event) => event.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t update unexisting task');
+    }
+
+    this.#events = [...this.#events.slice(0, index), update, ...this.#events.slice(index + 1)];
+    this._notify(updateType, update);
+  }
+
+  addEvent(updateType, update) {
+    this.#events = [update, ...this.#events];
+    this._notify(updateType, update);
+  }
+
+  deleteEvent(updateType, update) {
+    const index = this.#events.findIndex((event) => event.id === update.id);
+
+    if (index === -1) {
+      throw new Error('Can\'t delete unexisting task');
+    }
+
+    this.#events = [...this.#events.slice(0, index), ...this.#events.slice(index + 1)];
+    this._notify(updateType);
   }
 
   init() {
