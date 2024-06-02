1cf0be9de06a6face1e1291b8c6c8550948c8a7a
Irina Lagutina <ivl77@yandex.ru>
2024-06-02 20:23:20 +0300

    Удалила ненужные константы

--- src/consts.js
+++ src/consts.js
@@ -1,10 +1,7 @@
 const EVENT_TYPES = ['taxi', 'bus', 'train', 'ship', 'drive', 'flight', 'check-in', 'sightseeing', 'restaurant'];
 const EVENT_DEFAULT_TYPE = 'flight';
-const PLACES = ['Moscow', 'Geneva', 'Berlin', 'London', 'Abu-Dabi', 'New York', 'Paris'];
-const EVENTS_NUMBER = 4;
 
 const NEW_EVENT = {
-  id: null,
   basePrice: 0,
   dateFrom: null,
   dateTo: null,
@@ -35,4 +32,4 @@ const Loading = {
   IN_PROGRESS: 'progress',
 };
 
-export { EVENT_TYPES, PLACES, EVENTS_NUMBER, NEW_EVENT, HOURS_IN_DAY, MINUTES_IN_HOUR, EVENT_DEFAULT_TYPE, UserAction, UpdateType, Loading };
+export { EVENT_TYPES, NEW_EVENT, HOURS_IN_DAY, MINUTES_IN_HOUR, EVENT_DEFAULT_TYPE, UserAction, UpdateType, Loading };
